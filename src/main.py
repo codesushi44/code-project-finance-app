3d8374767073b567a9ecaa7263f2cba022365c90
codesushi44 <leejinyao1113@gmail.com>
2026-03-25 21:40:17 +0800

    Setup environment and testing sample main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Finance App Started")
+import flet as ft
+
+def main(page: ft.Page):
+    # 设置窗口标题
+    page.title = "Finance App Test"
+    # 在页面加一行字
+    page.add(ft.Text("Hello! 你的财务 App 启动成功了。"))
+
+# 启动应用
+ft.run(main)
